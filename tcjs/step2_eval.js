a8f3f0795f5201984a54916da3758593b6f224d7
Thomas Christensen <thomas@chrstnsn.dk>
2015-02-02 22:18:21 +0100

    eval with env (+ - * /)

---
+++ tcjs/step2_eval.js
@@ -0,0 +1,91 @@
+var reader = require('./reader.js'),
+    read_str = reader.read_str;
+
+var types = require('./types');
+
+function READ(a) {
+    return read_str(a);
+}
+
+function eval_ast(ast, env) {
+    if (types.isSymbol(ast)) {
+        var name = types.nameOf(ast);
+        var el = env[name];
+
+        if (!el) {
+            throw new Error("error looking up symbol: " + name);
+        }
+        return el;
+    }
+    if (types.isList(ast)) {
+        var res = ast.map(function (x) {
+            return EVAL(x, env);
+        });
+        types.toList(res);
+        return res;
+    }
+    return ast;
+}
+
+function EVAL(ast, env) {
+    if (types.isList(ast)) {
+        var evaled = eval_ast(ast, env);
+        var func = evaled.shift();
+        return func.apply(undefined, evaled);
+    }
+    return eval_ast(ast, env);
+}
+
+var pr_str = require('./printer.js').pr_str;
+
+function PRINT(a) {
+    return pr_str(a);
+}
+
+function DEBUG_rep(a) {
+    var r = READ(a);
+    console.log("read:  ", r);
+
+    var e = EVAL(r);
+    console.log("eval:  ", e);
+
+    var p = PRINT(e);
+    console.log("print: ", p);
+
+    return p;
+}
+
+var floorIt = Math.floor;
+
+var repl_env = {'+': function (a, b) { return floorIt(a + b);},
+                '-': function (a, b) { return floorIt(a - b);},
+                '*': function (a, b) { return floorIt(a * b);},
+                '/': function (a, b) { return floorIt(a / b);}
+               };
+
+function rep(a) {
+    return PRINT(EVAL(READ(a), repl_env));
+}
+
+var readline = require('./node_readline.js');
+
+if (process.argv[2] === 'debug') {
+    console.log("debugging REPL");
+    rep = DEBUG_rep;
+}
+
+// repl loop
+if (typeof require !== 'undefined' && require.main === module) {
+    // Synchronous node.js commandline mode
+    while (true) {
+        var line = readline.readline("mal-user> ");
+        if (line === null) { break; }
+        try {
+            if (line) { console.log(rep(line)); }
+        } catch (exc) {
+            if (exc instanceof reader.BlankException) { continue; }
+            if (exc.stack) { console.log(exc.stack); }
+            else           { console.log(exc); }
+        }
+    }
+}
